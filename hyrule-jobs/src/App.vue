3426b5bc4bd4cc8e980d1269fc3d03dc6f83f4be
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-04 14:44:36 +0800

    #3 -ref - Composition API & TypeScript
    #3 -ref - Composition API & TypeScript

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -7,22 +7,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
 
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup(){
-    const state = reactive({
-      name: 'Link',
-      age: 25 as string | number
-    })
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
 
-    state.name = '999' //cannot change type
-    state.age = 26
+    // state.name = '999' //cannot change type
+    // state.age = 26
 
-    return { ...toRefs(state) }
+    // return { ...toRefs(state) }
+    const name = ref('Link')
+    const age = ref<number | string>(100)
+
+    return { name, age}
   },
   methods: {
     changeName(name: string){
