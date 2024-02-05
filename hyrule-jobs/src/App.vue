494d0d02b63f3d57cdf1e4cbe1d1907b53ef6ad5
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-05 19:52:53 +0800

    #7 - Hylia Font & Final Styles - DONE
    #7 - Hylia Font & Final Styles - DONE

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">Orber by Title</button>
     <button @click="handleClick('salary')">Orber by Salary</button>
@@ -57,20 +61,30 @@ export default defineComponent({
 
 <style>
 header {
-  text-align: center;
-}
-header .order{
-  margin-top: 20px;
-}
-button{
-  margin: 0 10px;
-  color: #1195c9;
-  border: 3px solid #1195c9;
-  background: #d5f0ff;
-  padding: 8px 16px;
-  border-radius: 4px;
-  cursor: pointer;
-  font-weight: bold;
-}
-
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
 </style>
